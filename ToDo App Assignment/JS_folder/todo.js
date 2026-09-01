8510c77890af51c32117bde20aca587f14115b3c
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-09-01 08:50:08 +0100

    Update todo.js

--- ToDo App Assignment/JS_folder/todo.js
+++ ToDo App Assignment/JS_folder/todo.js
@@ -44,8 +44,29 @@ function show() {
     /*This displays the tas as a list*/
     document.getElementById('todos').innerHTML = html;
 }
+
+/*this function removes the items from the list  */
+function remove() {
+    var od = this.getAttribute('id');
+    var todos = get_todos();
+
+    todos.splice(id, 1);
+
+    localStorage.setItem('todo', JSON.stringify(todos));
+    show();
+}
+
 /*This displays the inputed task when the 'Add Item' button is clicked*/
 document.getElementById('add').addEventListener('click', add);
+
+/*remove todo when the x button is clicked*/
+document.addEventListener('click', function(event) {
+    if (event.target.className === 'remove') {
+        remove.call(event.target);
+    }
+});
 /*this will keep the inputs displayed permantaly on the screen*/
 show();
+
+
 
